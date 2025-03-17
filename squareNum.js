ad68c55589dd13c38ba7e62de4e86e985cc46e6a
Nonna <nonna.arzumanyan.111@gmail.com>
2025-03-17 09:36:14 +0400

    the square of a number
    This function calculates the square of a number.

---
+++ squareNum.js
@@ -0,0 +1,7 @@
+function square(num) {
+	return num ** 2;
+}
+
+console.log("the square of 5 is:", square(5));
+console.log("the square of 8 is:", square(8));
+console.log("the square of 10 is:", square(10));
